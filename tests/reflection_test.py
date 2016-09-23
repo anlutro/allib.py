50f039560c4123bd49ac90045f1ab1ee7c0812a4
Andreas Lutro <anlutro@gmail.com>
2016-09-23 10:51:50 +0200

    no need to define function/class publicly

--- tests/reflection_test.py
+++ tests/reflection_test.py
@@ -1,15 +1,16 @@
 import allib.reflection
 
-def _func(a, b, c='c'):
-	pass
-
-class C(object):
-	def _func(a, b, c='c'):
-		pass
 
 def test_get_fn_argnames():
+	def f(a, b, c='c'):
+		pass
+
+	class C(object):
+		def f(a, b, c='c'):
+			pass
+
 	c = C()
-	for func in _func, c._func:
-		args, kwargs = allib.reflection.get_fn_argnames(_func)
+	for func in f, c.f:
+		args, kwargs = allib.reflection.get_fn_argnames(f)
 		assert ('a', 'b') == args
 		assert ('c',) == kwargs
