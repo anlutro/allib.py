d88316962da0239605b7f1a9845c52a3d7ce7c24
Andreas Lutro <anlutro@gmail.com>
2017-07-09 21:48:21 +0200

    more comments, and optimize a little bit

--- allib/flask.py
+++ allib/flask.py
@@ -4,21 +4,32 @@ import os.path
 
 def cache_busting_url_for(app, old_url_for, bust_extensions=True):
 	'''
-	Replace flask's url_for with one that supports cache busting of static files.
+	Replace flask's url_for with one that supports cache busting of static
+	files. Requires that you have your web server set up to rewrite these URLs,
+	i.e. "foo.12345678.css" should be rewritten to "foo.css". Example:
+
+	flask.url_for = cache_busting_url_for(
+		app, flask.url_for, app.config['CACHE_BUST_URLS']
+	)
 	'''
+
+	if not bust_extensions:
+		return old_url_for
 
 	def transform_filename(orig_filename):
 		file_path = os.path.join(app.static_folder, orig_filename)
 		if os.path.isfile(file_path):
-			timestamp = int(os.stat(file_path).st_mtime)
 			directory, filename = os.path.split(orig_filename)
 			filename, extension = filename.split('.', 1)
 			if bust_extensions is True or extension in bust_extensions or \
 					extension.split('.')[-1] in bust_extensions:
+				timestamp = int(os.stat(file_path).st_mtime)
 				filename = '{}.{}.{}'.format(filename, timestamp, extension)
 				return os.path.join(directory, filename)
 		return orig_filename
 
+	# keep a dict of rewritten URLs to prevent a `stat` every time url_for is
+	# called on a static file.
 	url_cache = {}
 
 	def url_for_wrapper(endpoint, **values):
