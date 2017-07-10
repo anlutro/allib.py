657d634def4d7cd721a527670d196108ed04982e
Andreas Lutro <anlutro@gmail.com>
2017-07-10 13:03:11 +0200

    work on cache busting code structure

--- tests/flask_test.py
+++ tests/flask_test.py
@@ -1,24 +1,35 @@
+import flask
 from allib.testing import mock
-from allib.flask import cache_busting_url_for
+from allib.flask import transform_filename, setup_cache_busting
 
 
-def test_cache_busting_url_for_with_static_url():
-	def stub_url_for(endpoint, **kwargs):
-		return 'static/' + kwargs['filename']
-	app = mock.Mock()
-	app.static_folder = '/path/to/static'
+def test_transform_filename():
+	static_dir = '/path/to/static'
 	stat_result = mock.Mock()
 	stat_result.st_mtime = 1234.0
-	f = cache_busting_url_for(app, stub_url_for, ['css'])
 	with mock.patch('os.path.isfile', return_value=True) as isfile, \
 	     mock.patch('os.stat', return_value=stat_result) as stat:
-		url = f('static', filename='file.css')
+		url = transform_filename(static_dir, 'file.css', True)
 		isfile.assert_called_once_with('/path/to/static/file.css')
 		stat.assert_called_once_with('/path/to/static/file.css')
-	assert 'static/file.1234.css' == url
+	assert 'file.1234.css' == url
 
-def test_cache_busting_url_for_with_route_url():
-	def stub_url_for(endpoint, **kwargs):
-		return endpoint + '/' + kwargs['foo']
-	url = cache_busting_url_for(mock.Mock(), stub_url_for)('test', foo='bar')
-	assert 'test/bar' == url
+
+def test_transform_filename_when_file_does_not_exist():
+	static_dir = '/path/to/static'
+	with mock.patch('os.path.isfile', return_value=False) as isfile:
+		url = transform_filename(static_dir, 'file.css', True)
+		isfile.assert_called_once_with('/path/to/static/file.css')
+	assert 'file.css' == url
+
+
+def test_app_url_defaults():
+	app = flask.Flask(__name__)
+	setup_cache_busting(app)
+	stat_result = mock.Mock()
+	stat_result.st_mtime = 1234.0
+	with mock.patch('os.path.isfile', return_value=True) as isfile, \
+	     mock.patch('os.stat', return_value=stat_result) as stat, \
+	     app.test_request_context():
+		url = flask.url_for('static', filename='file.css')
+	assert '/static/file.1234.css' == url
