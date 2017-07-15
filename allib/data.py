34030cbc7136de5470a161ee7e334d963e6c715f
Andreas Lutro <anlutro@gmail.com>
2017-07-15 15:04:02 +0200

    forgot to add deep copy logic

--- allib/data.py
+++ allib/data.py
@@ -2,7 +2,10 @@ import copy
 
 
 def deep_dict_merge(old_dict, new_dict, deep=True):
-	old_dict = copy.deepcopy(old_dict)
+	if deep:
+		old_dict = copy.deepcopy(old_dict)
+	else:
+		old_dict = old_dict.copy()
 	for key, value in new_dict.items():
 		if key in old_dict and isinstance(old_dict[key], dict) and isinstance(value, dict):
 			old_dict[key] = deep_dict_merge(old_dict[key], value, deep=False)
