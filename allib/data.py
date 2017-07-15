d34b2bedd26910efaf485bd12e274628e348372b
Andreas Lutro <anlutro@gmail.com>
2017-07-15 15:05:45 +0200

    avoid unnecessary copies

--- allib/data.py
+++ allib/data.py
@@ -1,14 +1,17 @@
-import copy
+from copy import deepcopy
 
 
-def deep_dict_merge(old_dict, new_dict, deep=True):
-	if deep:
-		old_dict = copy.deepcopy(old_dict)
-	else:
-		old_dict = old_dict.copy()
+def deep_dict_merge(old_dict, new_dict, copy=True, deep=True):
+	if copy:
+		if deep:
+			old_dict = deepcopy(old_dict)
+		else:
+			old_dict = old_dict.copy()
+
 	for key, value in new_dict.items():
 		if key in old_dict and isinstance(old_dict[key], dict) and isinstance(value, dict):
-			old_dict[key] = deep_dict_merge(old_dict[key], value, deep=False)
+			deep_dict_merge(old_dict[key], value, copy=False, deep=False)
 		else:
 			old_dict[key] = value
+
 	return old_dict
