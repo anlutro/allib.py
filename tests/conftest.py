3a849db715021e0ef52d4d2f0c88c16dcfdb1f9a
Andreas Lutro <anlutro@gmail.com>
2017-10-06 20:27:33 +0200

    use pytest_ignore_collect to ignore specific test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
-import pytest
 import os.path
+import sys
+import pytest
 
 @pytest.fixture()
 def test_files_dir():
 	return os.path.join(os.path.dirname(__file__), 'files')
+
+def pytest_ignore_collect(path, config):
+	if path.basename == 'di_test.py' and sys.version_info < (3, 3):
+		return True
+	return None
