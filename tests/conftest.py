0c7bc2e445e6571a80031c731b7aaea1af00ce22
Andreas Lutro <anlutro@gmail.com>
2017-10-11 20:24:19 +0200

    ignore failures on python2 yolo

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ def test_files_dir():
 	return os.path.join(os.path.dirname(__file__), 'files')
 
 def pytest_ignore_collect(path, config):
+	# di_test has python3-only syntax
 	if path.basename == 'di_test.py' and sys.version_info < (3, 3):
 		return True
-	return None
