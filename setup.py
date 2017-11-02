fc4a7f4cc738d81f321345002a6a76e581c50da9
Andreas Lutro <anlutro@gmail.com>
2017-11-02 14:00:00 +0100

    work on setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
+from setuptools import find_packages
+import os
+import os.path
 
+# allow setup.py to be ran from anywhere
+os.chdir(os.path.dirname(__file__))
 
 setup(
 	name='allib',
-	packages=['allib'],
 	version='0.3.0',
 	license='MIT',
 	description='Personal library of useful stuff.',
 	author='Andreas Lutro',
 	author_email='anlutro@gmail.com',
 	url='https://github.com/anlutro/allib.py',
+	packages=find_packages(include=('allib', 'allib.*')),
 )
