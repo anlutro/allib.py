99256614478c96de82fa30f4deea02cdc7f4eab2
Andreas Lutro <anlutro@gmail.com>
2018-01-24 22:47:26 +0100

    just give up on testing older python versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,7 @@
 import os.path
-import sys
 import pytest
+
 
 @pytest.fixture()
 def test_files_dir():
 	return os.path.join(os.path.dirname(__file__), 'files')
-
-def pytest_ignore_collect(path, config):
-	# di_test has python3-only syntax
-	if path.basename == 'di_test.py' and sys.version_info < (3, 3):
-		return True
