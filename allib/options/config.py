7cb0ecf94ac50ace562e25faf7079ae9bba9dbb2
Andreas Lutro <anlutro@gmail.com>
2018-04-24 22:47:23 +0200

    work on config

--- allib/options/config.py
+++ allib/options/config.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 import copy
 import os.path
 
+from allib.data import update_dict
+
 
 _STR_TYPES = (str, bytes)
 try:
@@ -12,14 +14,6 @@ except NameError:
 
 class ConfigError(Exception):
 	pass
-
-
-def _merge_defaults(defaults, confdict):
-	for key, value in defaults.items():
-		if key not in confdict:
-			confdict[key] = value
-		elif isinstance(value, dict) and isinstance(confdict[key], dict):
-			_merge_defaults(value, confdict[key])
 
 
 def _validate_value(value, valid_type, key):
@@ -69,36 +63,48 @@ def _str_to_type(value, valid_type):
 
 	return value
 
+class ConfigAssembler:
+	def __init__(self, args, defaults=None, types=None):
+		self.args = dict(args)
+		self.defaults = {}
+		self.defaults_callback = None
+		if callable(defaults):
+			self.defaults_callback = defaults
+		elif defaults:
+			self.defaults = defaults
+		self.types = types or {}
+		self._conf_files = {}
 
-def get_config(args, default_location=None, optional=True, defaults=None, types=None):
-	"""
-	args: A dict of command-line options
-	default_location: Path to config file if not specified in `args`
-	defaults: Either a dictionary of default configuration values, or a function
-		that will be invoked as `defaults(config, args)` after the initial
-		dictionary has been constructed.
-	types: A dictionary of types to validate the config against.
-	"""
-	path = args.get('config') or default_location
+	def add_conf_file(self, path):
+		self._conf_files[path] = self.conf_file_to_dict(path)
 
-	if not optional and not os.path.exists(path):
-		msg = 'Could not find a config file at path %r' % path
-		if not args.get('config'):
-			msg += '. Specify one with the -c/--config command line option.'
-		raise ConfigError(msg)
+	def assemble(self):
+		confdict = copy.deepcopy(self.defaults)
+		for conf in self._conf_files.values():
+			update_dict(confdict, conf)
 
-	def configparser_to_dict(config):
+		confdict.setdefault('logging', {})
+		if 'log_level' in self.args:
+			confdict['logging']['level'] = self.args['log_level']
+		if 'log_file' in self.args:
+			confdict['logging']['file'] = self.args['log_file']
+		if 'log_path' in self.args:
+			confdict['logging']['file'] = self.args['log_path']
+
+		return confdict
+
+	def configparser_to_dict(self, config):
 		"""
 		Transform a configparser object into a dictionary.
 		"""
-		confdict = copy.deepcopy(defaults) if defaults else {}
+		confdict = copy.deepcopy(self.defaults)
 
 		for section in config.sections():
 			confdict[section] = {}
 			for item, value in config.items(section):
 				valid_type = None
-				if types and section in types and item in types[section]:
-					valid_type = types[section][item]
+				if section in self.types and item in self.types[section]:
+					valid_type = self.types[section][item]
 				if valid_type is bool:
 					value = config.getboolean(section, item)
 				elif valid_type:
@@ -108,7 +114,7 @@ def get_config(args, default_location=None, optional=True, defaults=None, types=
 
 		return confdict
 
-	def conf_file_to_dict(path):
+	def conf_file_to_dict(self, path):
 		if path.endswith('.yml') or path.endswith('.yaml'):
 			import yaml
 			load = yaml.safe_load
@@ -125,36 +131,37 @@ def get_config(args, default_location=None, optional=True, defaults=None, types=
 				import ConfigParser as configparser
 			config = configparser.ConfigParser()
 			config.read(path)
-			return configparser_to_dict(config)
+			return self.configparser_to_dict(config)
 
 		with open(path) as file:
 			confdict = load(file)
-		if defaults:
-			if callable(defaults):
-				defaults(confdict, args)
-			else:
-				_merge_defaults(defaults, confdict)
-		if types:
-			_validate_dict(confdict, types)
+
+		if self.defaults_callback:
+			self.defaults_callback(confdict, self.args)
+
+		_validate_dict(confdict, self.types)
+
 		return confdict
 
+
+def get_config(args, default_location=None, optional=True, defaults=None, types=None):
+	"""
+	args: A dict of command-line options
+	default_location: Path to config file if not specified in `args`
+	defaults: Either a dictionary of default configuration values, or a function
+		that will be invoked as `defaults(config, args)` after the initial
+		dictionary has been constructed.
+	types: A dictionary of types to validate the config against.
+	"""
+	config_assembler = ConfigAssembler(args, defaults=defaults, types=types)
+
+	path = args.get('config') or default_location
+	if not optional and not os.path.exists(path):
+		msg = 'Could not find a config file at path %r' % path
+		if not args.get('config'):
+			msg += '. Specify one with the -c/--config command line option.'
+		raise ConfigError(msg)
 	if path:
-		confdict = conf_file_to_dict(path)
-	else:
-		if not optional:
-			raise ConfigError(
-				'Configuration file required! Specify one '
-				'with the -c/--config command line option.'
-			)
-		confdict = {}
+		config_assembler.add_conf_file(path)
 
-	if 'logging' not in confdict:
-		confdict['logging'] = {}
-	if 'log_level' in args:
-		confdict['logging']['level'] = args.pop('log_level')
-	if 'log_file' in args:
-		confdict['logging']['file'] = args.pop('log_file')
-	if 'log_path' in args:
-		confdict['logging']['file'] = args.pop('log_path')
-
-	return confdict
+	return config_assembler.assemble()
--- tests/config_test.py
+++ tests/config_test.py
@@ -1,10 +1,11 @@
+from unittest import mock
 import os.path
 
 from allib.options.config import get_config
 
+
 def test_parse_config(test_files_dir):
-	args = dict(config=None)
-	cfg = get_config(args, os.path.join(test_files_dir, 'config.cfg'))
+	cfg = get_config({}, os.path.join(test_files_dir, 'config.cfg'))
 	assert isinstance(cfg, dict)
 	assert cfg['sec1']['foo'] == 'bar'
 	assert cfg['sec1']['bar'] == 'baz'
@@ -12,8 +13,24 @@ def test_parse_config(test_files_dir):
 
 
 def test_parse_yaml(test_files_dir):
-	args = dict(config=None)
-	cfg = get_config(args, os.path.join(test_files_dir, 'config.yml'))
+	cfg = get_config({}, os.path.join(test_files_dir, 'config.yml'))
 	assert isinstance(cfg, dict)
 	assert cfg['foo'] == 'bar'
 	assert cfg['bar']['baz'] == 'foo'
+
+
+def test_defaults():
+	with mock.patch('allib.options.config.ConfigAssembler.conf_file_to_dict') as mock_file_to_dict:
+		mock_file_to_dict.return_value = {'conf-key': 'conf-val'}
+		cfg = get_config({}, '/does-not-matter', optional=True, defaults={
+			'conf-key': 'default-val',
+			'default-key': 'default-val',
+		})
+	assert cfg['conf-key'] == 'conf-val'
+	assert cfg['default-key'] == 'default-val'
+
+
+def test_log_args():
+	args = {'log_level': 'debug'}
+	cfg = get_config(args, None, optional=True)
+	assert cfg['logging']['level'] == 'debug'
